61fd1c8335abbada02bdb65904f50e8d3d9355ad
EricGoldstein2 <165966545+EricGoldstein2@users.noreply.github.com>
2025-02-13 12:13:03 -0800

    volume slider

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -6,27 +6,35 @@ public class MenuController : MonoBehaviour
 
     public GameObject m_Menu;
     public GameObject button;
+    public AudioSource click;
     public void OnStartButton()
     {
+        click.Play();
         SceneManager.LoadScene(1);
+        
     }
 
     public void OnQuitButton()
     {
         if (m_Menu.activeSelf == true)
         {
+            click.Play();
             m_Menu.SetActive(!m_Menu.activeSelf);
             button.SetActive(!button.activeSelf);
+          
         }
         else{
 
+            click.Play();
             Application.Quit();
         }
     }
 
     public void OnOptionsButton()
     {
+        click.Play();
         m_Menu.SetActive(!m_Menu.activeSelf);
         button.SetActive(!button.activeSelf);
+    
     }
 }
