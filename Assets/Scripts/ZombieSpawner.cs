2c83d0f0a8f14a6826e10b2fa510a5f5dfecf253
ishaThor <137206455+ishaThor@users.noreply.github.com>
2025-02-15 15:28:27 -0800

    Added Zombie Counter
    Zombie Counter and improves random spawn method

--- Assets/Scripts/ZombieSpawner.cs
+++ Assets/Scripts/ZombieSpawner.cs
@@ -1,16 +1,27 @@
+using TMPro;
+using Unity.AI.Navigation;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class ZombieSpawner : MonoBehaviour
 {
     public GameObject zombiePrefab;
+    //zombie counter on screen
+    private TextMeshProUGUI _zombieCounter;
     [SerializeField] int numberOfZombies;
     private int[] xBoundary = new int[2];
-    private int[] zBoundary = new int[2]; 
-    
+    private int[] zBoundary = new int[2];
+    public NavMeshSurface ground;
+
+    private void Awake()
+    {
+        _zombieCounter = GetComponentInChildren<TextMeshProUGUI>();
+    }
     public void spawnZombies(int wave)
     {
         // Number of zombies increases by 5 per wave
         numberOfZombies = wave * 5; 
+        _zombieCounter.text = "Zombies Left: " + numberOfZombies.ToString();
         
         // Zombie spawn region for wave (will change/expand when we have more waves) 
         if (wave == 1)
@@ -30,18 +41,41 @@ public class ZombieSpawner : MonoBehaviour
 
         }
 
-        for (int i = 0; i < numberOfZombies; i++)
+        int i = 0;
+        while (i < numberOfZombies)
         {
             // Random spawn position for zombies within x, z bounds (can change later) 
 
             Vector3 spawnPosition = new Vector3(Random.Range(xBoundary[0], xBoundary[1]), 2.5f, Random.Range(zBoundary[0], zBoundary[1]));
-            Instantiate(zombiePrefab, spawnPosition, Quaternion.identity);
+            NavMeshHit hit;
+
+            if (NavMesh.SamplePosition(spawnPosition, out hit, 2f, NavMesh.AllAreas))
+            {
+                // Check if this point is inside a NavMeshObstacle
+                if (!IsInsideNavMeshObstacle(hit.position))
+                {
+                    Instantiate(zombiePrefab, hit.position, Quaternion.identity);
+                    i++;
+                   
+                    
+                }
+            }
         }
+    }
+
+    bool IsInsideNavMeshObstacle(Vector3 position)
+    {
+        NavMeshHit hit;
+
+        // will need to be changed if we ever have floating NavMesh Obstacle
+        bool isBlocked = NavMesh.Raycast(position + Vector3.up * 2f, position - Vector3.up * 2f, out hit, NavMesh.AllAreas);
+        return isBlocked;
     }
 
     public void zombieCounterDecrement()
     {
         numberOfZombies--;
+        _zombieCounter.text = "Zombies Left: " + numberOfZombies.ToString();
         Debug.Log("There are " + numberOfZombies + " zombies left.");
         if (numberOfZombies == 0)
         {
