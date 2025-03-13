e9ed7c64199add75e5c5829c8e5c94a30781827c
Gabe-Alonso <gabixtreme363@outlook.com>
2025-03-13 16:48:06 -0700

    Grenade implemented, Mildly funky

--- Assets/Scripts/Zombie Scripts/ZombieFollow.cs
+++ Assets/Scripts/Zombie Scripts/ZombieFollow.cs
@@ -269,16 +269,10 @@ public class ZombieFollow : MonoBehaviour
         //IF collision is from a BULLET
         if (collision.gameObject.tag == "Bullet")
         {
+            Debug.Log("DAMAGED");
             takeDamage();
         }
 
-
-        //IF Collision is from the PLAYER
-        if (collision.gameObject.tag == "Player")
-        {
-            //Access the Player's Health here some how
-            //collision.gameObject.health--;
-        }
     }
 
     void OnTriggerEnter(Collider other)
